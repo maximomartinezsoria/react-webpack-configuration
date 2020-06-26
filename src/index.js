5297768c6be992b8c9de47f18b4c8da86f8aa9f7
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2020-06-26 14:51:48 -0300

    Installing React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello, World!')
+import './sass/index.scss'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+
+render(<App/>, document.querySelector('#app'))
